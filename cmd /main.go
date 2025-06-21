590d41ce388f1f6519db2b3dea733d04500ec882
temukuev <uncreated404@gmail.com>
2025-06-21 18:17:49 +0300

    implemented crud for basic tasks

--- cmd /main.go
+++ cmd /main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"os"
+	"pomodoro-timer/internal/handlers"
+	"pomodoro-timer/internal/middleware"
 	"pomodoro-timer/internal/models"
 
 	"github.com/gin-gonic/gin"
@@ -22,7 +24,26 @@ func main() {
 	}
 
 	r := gin.Default()
-	
+
+	r.POST("/register", handlers.Register(db))
+	r.POST("/login", handlers.Login(db))
+
+	taskGroup := r.Group("/tasks")
+	taskGroup.Use(middleware.JWTAuth())
+	taskGroup.POST("", handlers.CreateTask(db))
+	taskGroup.GET("", handlers.GetTasks(db))
+	taskGroup.PUT(":id", handlers.UpdateTask(db))
+	taskGroup.DELETE(":id", handlers.DeleteTask(db))
+
+	pomodoroGroup := r.Group("/pomodoro")
+	pomodoroGroup.Use(middleware.JWTAuth())
+	pomodoroGroup.POST("", handlers.CreatePomodoroSession(db))
+	pomodoroGroup.GET("", handlers.GetPomodoroSessions(db))
+	pomodoroGroup.PUT(":id", handlers.UpdatePomodoroSession(db))
+	pomodoroGroup.DELETE(":id", handlers.DeletePomodoroSession(db))
+
+	r.GET("/analytics/tasks-time", middleware.JWTAuth(), handlers.TasksTimeAnalytics(db))
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
